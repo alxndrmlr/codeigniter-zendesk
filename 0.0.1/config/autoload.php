2a16190e7e25858a74005f2d51ac4af4685a4ebd
Alexander Millar <alexander.millar@gmail.com>
2013-08-23 16:18:45 -0400

    Initial commit

---
+++ 0.0.1/config/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+# Load the codeigniter-zendesk config when the spark is loaded
+$autoload['config'] = array('codeigniter-zendesk');
+
+# Load the codeigniter-zendesk helper when the spark is loaded
+$autoload['helper'] = array('codeigniter-zendesk');
---
+++ 0.0.1/config/codeigniter-zendesk.php
@@ -0,0 +1,8 @@
+<?php
+
+# Config for Zendesk API
+
+$config['z_APIKEY'] = '';
+$config['z_USER'] = '';
+$config['z_SUBDOMAIN'] = '';
+
